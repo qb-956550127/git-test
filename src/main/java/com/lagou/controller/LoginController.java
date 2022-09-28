3e18284e39f7767049528264ee661558eb75d728
champion <956550127@qq.com>
2022-09-28 15:40:29 +0800

    commit 4

--- src/main/java/com/lagou/controller/LoginController.java
+++ src/main/java/com/lagou/controller/LoginController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * 处理登录业务gitceshibranch--master-branch
+ * 处理登录业务gitceshibranch--master-branch--back
  */
 @Controller
 public class LoginController {
