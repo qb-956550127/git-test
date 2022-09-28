4ca948adf65f97911e1879b81f839c2b61c4d292
champion <956550127@qq.com>
2022-09-28 17:17:18 +0800

    777788gitssh

--- src/main/java/com/lagou/controller/LoginController.java
+++ src/main/java/com/lagou/controller/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * 处理登录业务gitceshibranch--master-branch--back
  * 处理登录业务gitceshibranch--master-branch-pull
- * gayaozi--ganiyaozi-https--ssh
+ * gayaozi--ganiyaozi-https--ssh--gitssh
  */
 @Controller
 public class LoginController {
