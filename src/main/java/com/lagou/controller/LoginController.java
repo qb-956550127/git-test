8d041c082bb5f7c861065b00f1fa60422437f82f
qb-956550127 <76278596+qb-956550127@users.noreply.github.com>
2022-09-28 16:17:40 +0800

    Update LoginController.java

--- src/main/java/com/lagou/controller/LoginController.java
+++ src/main/java/com/lagou/controller/LoginController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * 处理登录业务gitceshibranch--master-branch
+ * 处理登录业务gitceshibranch--master-branch-pull
  */
 @Controller
 public class LoginController {
